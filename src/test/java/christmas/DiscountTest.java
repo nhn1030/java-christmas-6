9a459ed5d5cce6119343ecfb664d87733e77603b
nhn1030 <shgusska12@gmail.com>
2023-11-14 18:43:52 +0900

    test: christmasDiscount 메서드 구현을 위한 테스트 코드 작성

--- src/test/java/christmas/DiscountTest.java
+++ src/test/java/christmas/DiscountTest.java
@@ -1,2 +1,15 @@
-package christmas;public class DiscountTest {
+package christmas;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.time.LocalDate;
+
+public class DiscountTest {
+    @Test
+    // 크리스마스 할인이 올바르게 계산되는지 테스트
+    void testChristmasDiscount() {
+        LocalDate december2 = LocalDate.of(2023, 12, 1);
+        int discountDicember2 = Discount.christmasDiscount(december2);
+        assertEquals(1000, discountDicember2);
+    }
 }
