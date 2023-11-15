404d55dda6f7fc413400d61984fc5967af0ce184
nhn1030 <shgusska12@gmail.com>
2023-11-15 16:58:50 +0900

    feat: IndividualOrder 클래스 정의 및 addItemToOrder 메서드 구현 - getMenuItemByName에 전달된 값이 메뉴목록에 없는 경우에 get메소드는 해당 키에 대한 값이없기때문에 null을 반환 - null을 반환한다면 예외처리

---
+++ src/main/java/christmas/model/IndividualOrder.java
@@ -0,0 +1,17 @@
+package christmas.model;
+
+import christmas.dto.MenuDTO;
+import java.util.HashMap;
+import java.util.Map;
+
+public class IndividualOrder {
+    private final Map<String, Integer> orderedItems = new HashMap<>();
+    private final RestaurantMenu menu = new RestaurantMenu();
+
+    public void addItemToOrder(String menuName, int quantity) {
+        if (menu.getMenuItemByName(menuName) == null) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
+        orderedItems.put(menuName, quantity);
+    }
+}
