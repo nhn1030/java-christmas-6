4aad0dedddec329a777d7656aafc51161a7cf907
nhn1030 <shgusska12@gmail.com>
2023-11-15 17:03:13 +0900

    feat: 1 미만의 숫자가 입력될 경우에 대한 예외처리 코드 추가

--- src/main/java/christmas/model/IndividualOrder.java
+++ src/main/java/christmas/model/IndividualOrder.java
@@ -12,6 +12,11 @@ public class IndividualOrder {
         if (menu.getMenuItemByName(menuName) == null) {
             throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
         }
+
+        if (quantity <= 0) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
+
         orderedItems.put(menuName, quantity);
     }
 }
