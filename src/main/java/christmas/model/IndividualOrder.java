4862fd1785b74ed69c906246464fe8ddc40c6199
nhn1030 <shgusska12@gmail.com>
2023-11-15 22:58:12 +0900

    feat: IndividualOrder의 메뉴가 음료만을 제공한다면 true를 반환하는 메서드 구현

--- src/main/java/christmas/model/IndividualOrder.java
+++ src/main/java/christmas/model/IndividualOrder.java
@@ -7,12 +7,14 @@ import java.util.Map;
 
 public class IndividualOrder {
     private static final String INVALID_ORDER_ERROR = "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.";
+    private static final String DUPLICATE_ORDER_ERROR = "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.";
+    private static final String ONLY_BEVERAGE_ORDER_ERROR = "[ERROR] 음료만 주문하는 것은 불가능 합니다. 다시 입력해 주세요.";
     private final Map<String, Integer> orderedItems = new HashMap<>();
     private final RestaurantMenu menu;
 
     public IndividualOrder(RestaurantMenu menu) {
         if (menu == null) {
-            throw new IllegalArgumentException("Menu cannot be null.");
+            throw new IllegalArgumentException("[ERROR] Menu cannot be null.");
         }
         this.menu = menu;
     }
@@ -24,9 +26,11 @@ public class IndividualOrder {
         if (!isValidOrder(menuName, quantity)) {
             throw new IllegalArgumentException(INVALID_ORDER_ERROR);
         }
-
         else if (!isDuplicate(menuName)) {
-            throw new IllegalArgumentException(INVALID_ORDER_ERROR);
+            throw new IllegalArgumentException(DUPLICATE_ORDER_ERROR);
+        }
+        else if (checkBeverageOrder(orderDTO)) {
+            throw new IllegalArgumentException(ONLY_BEVERAGE_ORDER_ERROR);
         }
 
         orderedItems.put(menuName, quantity);
@@ -47,7 +51,13 @@ public class IndividualOrder {
         return totalPrice;
     }
 
+    public boolean checkBeverageOrder(OrderDTO orderDTO) {
+        String menuName = orderDTO.getMenuName();
+        MenuDTO menuDTO = menu.getMenuItemByName(menuName);
 
+        // 음료인 경우 true를 반환, 아닌 경우 false를 반환
+        return menuDTO != null && menuDTO.getCategory() == RestaurantMenu.MenuCategory.BEVERAGE;
+    }
 
     private boolean isValidOrder(String menuName, int quantity) {
         return menu.getMenuItemByName(menuName) != null && quantity > 0;
