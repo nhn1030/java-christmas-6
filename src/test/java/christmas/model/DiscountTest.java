d89d3eb227a46710ec92168f068d68f62705aea2
nhn1030 <shgusska12@gmail.com>
2023-11-15 13:13:55 +0900

    test: 테스트 클래스 패키지 이동 - model 패키지로 이동

--- src/test/java/christmas/model/DiscountTest.java
+++ src/test/java/christmas/model/DiscountTest.java
@@ -1,6 +1,5 @@
-package christmas;
+package christmas.model;
 
-import christmas.model.Discount;
 import java.time.LocalDate;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
