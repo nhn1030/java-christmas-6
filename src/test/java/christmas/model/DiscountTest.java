ba4522fa9cd29c64796f74e7c87cff097fe07cae
nhn1030 <shgusska12@gmail.com>
2023-11-15 13:15:07 +0900

    test: 테스트 클래스 리팩토링 - 불필요한 주석 제거

--- src/test/java/christmas/model/DiscountTest.java
+++ src/test/java/christmas/model/DiscountTest.java
@@ -6,7 +6,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DiscountTest {
     @Test
-        // 크리스마스 할인이 올바르게 계산되는지 테스트
     void testChristmasDiscount() {
         LocalDate december25 = LocalDate.of(2023, 12, 25);
         int discountDecember25 = Discount.christmasDiscount(december25);
