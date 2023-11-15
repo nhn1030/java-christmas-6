f58cd73729ebfad05faae449bcf720d54f93d73f
nhn1030 <shgusska12@gmail.com>
2023-11-15 16:23:04 +0900

    test: IndividualOrder 클래스와 calculateTotalPrice 메서드 구현을 위한 테스트 코드 정의

---
+++ src/test/java/christmas/model/IndividualOrderTest.java
@@ -0,0 +1,24 @@
+package christmas.model;
+
+import christmas.dto.MenuDTO;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+public class IndividualOrderTest {
+    @Test
+    void testCalculateTotalPrice() {
+        IndividualOrder order = new IndividualOrder();
+
+        // 가격이 일치하는지 확인할 메뉴
+        String matchMenuName = "티본스테이크";
+        int OrderQuantity = 2;
+
+        // 메뉴를 주문에 추가
+        order.addItemToOrder(matchMenuName, OrderQuantity);
+
+        // 해당하는 메뉴의 menuDTO를 가져와 주문가격이 일치하는 지 확인
+        MenuDTO menuDTO = new RestaurantMenu().getMenuItemByName(matchMenuName);
+        int expectedTotalPrice = menuDTO.getPrice() * OrderQuantity;
+
+        assertEquals(expectedTotalPrice, order.calculateTotalPrice());
+    }
+}
