b0534c1cb1491fe65d1c1a64389a9ac310371289
nhn1030 <shgusska12@gmail.com>
2023-11-15 13:12:32 +0900

    refactor:christmasDiscount 메서드 리팩토링 - 상수 Enum으로 관리되도록 수정

--- src/main/java/christmas/model/Discount.java
+++ src/main/java/christmas/model/Discount.java
@@ -4,16 +4,25 @@ import java.time.LocalDate;
 
 public class Discount {
 
-    private static final int BASE_DISCOUNT = 1000;
-    private static final int DAILY_INCREASE = 100;
-    private static final int MAX_DISCOUNT = 3400;
-    private static final int DAYS_OFFSET = 1;
+    public enum DiscountConstants {
+        BASE_DISCOUNT(1000),
+        DAILY_INCREASE(100),
+        MAX_DISCOUNT(3400),
+        DAYS_OFFSET(1);
+
+        private final int value;
+
+        DiscountConstants(int value) {
+            this.value = value;
+        }
+    }
 
     // 크리스마스 할인 계산 메서드
     public static int christmasDiscount(LocalDate visitDate) {
-        int daysUntilChristmas = visitDate.getDayOfMonth() - DAYS_OFFSET;
-        int discountAmount = BASE_DISCOUNT + (daysUntilChristmas * DAILY_INCREASE);
+        int daysUntilChristmas = visitDate.getDayOfMonth() - DiscountConstants.DAYS_OFFSET.value;
+        int discountAmount = DiscountConstants.BASE_DISCOUNT.value +
+                (daysUntilChristmas * DiscountConstants.DAILY_INCREASE.value);
 
-        return Math.min(discountAmount, MAX_DISCOUNT);
+        return Math.min(discountAmount, DiscountConstants.MAX_DISCOUNT.value);
     }
 }
