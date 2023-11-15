2fe41c61f8e62fe88855093e15c9f954e6bd1a8d
nhn1030 <shgusska12@gmail.com>
2023-11-15 21:56:34 +0900

    test: 카테고리별 메뉴 값 반환 메서드 구현을 위한 테스트 코드 작성

--- src/test/java/christmas/model/RestaurantMenuTest.java
+++ src/test/java/christmas/model/RestaurantMenuTest.java
@@ -1,11 +1,23 @@
 package christmas.model;
 
 import christmas.dto.MenuDTO;
+import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+
 import static org.junit.jupiter.api.Assertions.*;
 
 
+
+
 public class RestaurantMenuTest {
+    private RestaurantMenu menuItem;
+    @BeforeEach
+    public void setUp() {
+        menuItem = new RestaurantMenu();
+    }
+
     @Test
     void testGetMenuItem_ExistingMenu() {
         RestaurantMenu menu = new RestaurantMenu();
@@ -15,5 +27,20 @@ public class RestaurantMenuTest {
 
         assertNotNull(menuDTO);
         assertEquals(menuName, menuDTO.getName());
+    }
+
+
+
+    @Test
+    public void testGetMenuByCategory() {
+        RestaurantMenu.MenuCategory categoryToTest = RestaurantMenu.MenuCategory.APPETIZER;
+
+        Map<String, MenuDTO> result = menuItem.getMenuByCategory(categoryToTest);
+
+
+        assertEquals(3, result.size());
+        assertEquals("양송이수프", result.get("양송이수프").getName());
+        assertEquals("타파스", result.get("타파스").getName());
+        assertEquals("시저샐러드", result.get("시저샐러드").getName());
     }
 }
