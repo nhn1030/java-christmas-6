858122afb9ffc44332ea05e663cba5f880e5d217
nhn1030 <shgusska12@gmail.com>
2023-11-15 22:32:44 +0900

    feat: MenoDTO category 추가

--- src/main/java/christmas/dto/MenuDTO.java
+++ src/main/java/christmas/dto/MenuDTO.java
@@ -1,12 +1,16 @@
 package christmas.dto;
 
+import christmas.model.RestaurantMenu.MenuCategory;
+
 public class MenuDTO {
     private final String name;
     private final int price;
+    private final MenuCategory category;
 
-    public MenuDTO(String name, int price) {
+    public MenuDTO(String name, int price, MenuCategory category) {
         this.name = name;
         this.price = price;
+        this.category = category;
     }
 
     public String getName() {
@@ -15,5 +19,9 @@ public class MenuDTO {
 
     public int getPrice() {
         return price;
+    }
+
+    public MenuCategory getCategory() {
+        return category;
     }
 }
