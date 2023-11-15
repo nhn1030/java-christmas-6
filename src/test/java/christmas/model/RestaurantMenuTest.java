f0fddc6d39232b80cc271c53ea7a74200790ed81
nhn1030 <shgusska12@gmail.com>
2023-11-15 15:53:04 +0900

    test: testGetMenuItem_NonExistingMenu 테스트 케이스 정의 - 존재하지 않는 메뉴이름을 전달한다면 예외 상황을 발생시키는지 테스트 - 예외 메시지가 [ERROR] 존재하지 않는 메뉴입니다. 인지 확인

--- src/test/java/christmas/model/RestaurantMenuTest.java
+++ src/test/java/christmas/model/RestaurantMenuTest.java
@@ -16,4 +16,15 @@ public class RestaurantMenuTest {
         assertNotNull(menuDTO);
         assertEquals(menuName, menuDTO.getName());
     }
+
+    @Test
+    void testGetMenuItem_NonExistingMenu() {
+        RestaurantMenu menu = new RestaurantMenu();
+        String menuName = "";
+
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+                () -> menu.getMenuItemByName(menuName));
+
+        assertEquals("[ERROR] 존재하지 않는 메뉴입니다.", exception.getMessage());
+    }
 }
