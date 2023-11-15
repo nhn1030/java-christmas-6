39b3525e87958e0c6e2e3e1e24b4b7be89df619b
nhn1030 <shgusska12@gmail.com>
2023-11-15 20:48:26 +0900

    feat: EventBadge 클래스를 위한 DTO 객체 정의

---
+++ src/main/java/christmas/dto/BadgeDTO.java
@@ -0,0 +1,13 @@
+package christmas.dto;
+
+public class BadgeDTO {
+    private String badgeName;
+
+    public BadgeDTO(String badgeName) {
+        this.badgeName = badgeName;
+    }
+
+    public String getBadgeName() {
+        return badgeName;
+    }
+}
